beffa6ca3146572504860bb53493521e9119bd0f
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2022-05-03 17:42:11 +0200

    Removed concat (to test)

--- deploy/global.js
+++ deploy/global.js
@@ -7,12 +7,11 @@ const LoadCommands = require("../util/loadCommands");
 	const config = await getConfig();
 	const rest = new REST({ version: "9" }).setToken(config.token);
 	const commands = await LoadCommands().then((cmds) => {
-		return [].concat(cmds.slash);
+		return cmds.slash;
 	});
 	
 	console.log("Deploying commands to global...");
-	await rest
-	.put(Routes.applicationCommands(config.clientId), {
+	await rest.put(Routes.applicationCommands(config.clientId), {
 		body: commands,
 	})
 	.catch(console.log);
