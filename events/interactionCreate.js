de6dfe845a5de6ca98037069669a8b785ac9420c
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2022-06-12 15:14:05 +0200

    update permissions embed

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const fuzzysort = require('fuzzysort')
 const { getPosition, levDistance } = require('../util/stringUtil');
 
@@ -8,7 +9,7 @@ const { getPosition, levDistance } = require('../util/stringUtil');
 // @interactionCreate: [interaction: Interaction];
 // This module checks some properties of the command and determines if it should be ran for that user or not
 module.exports = async (client, interaction) => {
-	
+
 	// Autocomplete handler, takes autocomplete options specified in the command properties 
 	// and shows them to the user
 	// node_modules\discord.js\src\structures\AutocompleteInteraction.js
@@ -19,23 +20,23 @@ module.exports = async (client, interaction) => {
 		const index = interaction.options._hoistedOptions.map(option => option.focused).indexOf(true);
 		// Gets the autocomplete options provided by the command
 		let options = await client.slash.get(interaction.commandName).autocompleteOptions(input, index, interaction);
-		
+
 		// This should make the algorithm faster by pre preparing the array, but no noticable changes
-		options.forEach(option => option.filePrepared = fuzzysort.prepare(option.name)); 
+		options.forEach(option => option.filePrepared = fuzzysort.prepare(option.name));
 		options.map(option => option.filePrepared);
 
 		fuzzysort.go(input, options, {
 			threshold: -10000, // Don't return matches worse than this (higher is faster)
 			limit: 30, // Don't return more results than this (lower is faster)
 			all: false, // If true, returns all results for an empty search
-		  
+
 			key: 'name', // For when targets are objects
 		})
-		
+
 		// Avoiding calculating levenshteing distances if it's not needed
 		if (options.length > 1) {
 			// Assigns Levenshtein distances for each option based on what the user is currently typing
-			for (let option of options) { 
+			for (let option of options) {
 				// I use '⟨⟨' and '⟩⟩' to delimit some "additional information" for options, 
 				// which shouldn't be looked at in the search, so I only base the levenshtein distance on everything
 				// before those characters
@@ -44,10 +45,9 @@ module.exports = async (client, interaction) => {
 			// Sorts the array of options and displays it according to the Levenshtein distance from the typed value
 			options.sort((a, b) => a.levenshteinDistance - b.levenshteinDistance)
 		}
-
 		interaction.respond(options.slice(0, 24));
 	}
-	
+
 	// Gets general info from a command during execution, if sent then check the guards
 	// run only if everything is valid
 	if (interaction.isCommand()) {
@@ -55,27 +55,33 @@ module.exports = async (client, interaction) => {
 		if (!command || !command.run) {
 			return interaction.reply("Sorry the command you used doesn't have any run function");
 		}
-		
+
 		if (command.ownerOnly && !client.config.ownerId.includes(interaction.user.id)) {
 			return interaction.reply({ content: "This command is only for the bot developers!", ephemeral: true });
 		}
 
 		if (command.permissions) {
 			let missingUserPerms = [];
-			let missingBotPerms = [];
+			let missingBotPerms = []
+
+			let missingPermsEmbed = new MessageEmbed().setColor(client.config.embedColor);
 			command.permissions.forEach(permission => {
 				if (!interaction.guild.members.cache.get(interaction.member.user.id).permissions.has(permission))
-				missingUserPerms.push("`" + permission + "`");
+					missingUserPerms.push("`" + permission + "`");
 				if (!interaction.guild.me.permissions.has(permission))
-				missingBotPerms.push("`" + permission + "`");
+					missingBotPerms.push("`" + permission + "`");
 			});
-			
-			if (missingUserPerms)
-			return interaction.reply({ content: `You do not have the required permissions for this command:\n${missingUserPerms.join(", ")}`, ephemeral: true})
-			if (missingBotPerms)
-			return interaction.reply({ content: `I need these permissions for this command:\n${missingBotPerms.join(", ")}`, ephemeral: true})
+
+			if (missingUserPerms.length || missingBotPerms.length) {
+				if (missingUserPerms.length)
+					missingPermsEmbed.addField("You're missing some permissions:", `${missingUserPerms.join(", ")}`)
+				if (missingBotPerms.length)
+					missingPermsEmbed.addField("I'm missing some permissions:", `${missingBotPerms.join(", ")}`)
+				missingPermsEmbed.setFooter({ text: "If you think this is a mistake please contact the manager of this bot in this server." })
+				return interaction.reply({ embeds: [missingPermsEmbed], ephemeral: true })
+			}
 		}
-		
+
 		const args = [];
 		for (let option of interaction.options.data) {
 			if (option.type === 'SUB_COMMAND') {
