f7ea29afb4ef1ed5ff1a4b479d3ca90239a89b20
Brian Ferri <81859084+BioCla@users.noreply.github.com>
2022-07-03 20:19:11 +0200

    comments

--- util/stringUtil.js
+++ util/stringUtil.js
@@ -1,3 +1,24 @@
+/*
+Example Levenshtein matrix
+			[j]	""	A	L	B	E	R	O > T
+	┌─────────┬───┬───┬───┬───┬───┬───┬───┐
+ S	│ (index) │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │
+ \/	├─────────┼───┼───┼───┼───┼───┼───┼───┤
+ ""	│    0    │ 0 │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │
+ L	│    1    │ 1 │ 1 │ 1 │ 2 │ 3 │ 4 │ 5 │
+ I	│    2    │ 2 │ 2 │ 2 │ 2 │ 3 │ 4 │ 5 │
+ B	│    3    │ 3 │ 3 │ 3 │ 2 │ 3 │ 4 │ 5 │
+ R	│    4    │ 4 │ 4 │ 4 │ 3 │ 3 │ 3 │ 4 │
+ O	│    5    │ 5 │ 5 │ 5 │ 4 │ 4 │ 4 │ 3 │
+[i]	└─────────┴───┴───┴───┴───┴───┴───┴───┘
+Iteration goes left->right - top->bottom
+
+considering the costs:
+insert : 1
+remove : 1
+replace : 1
+none : 0
+*/
 
 /**
  * Determines the Levenshtein distance between two given strings
@@ -6,19 +27,28 @@
  * @returns {number}
  */
 const levDistance = (S = '', T = '') => {
+	// iterator indexes
 	let i, j;
 	const n = S.length, m = T.length;
-	let L = Array(n+1).fill().map(() => Array(m+1).fill());
-	for (i=0; i < n+1; i++) {
-		for (j=0; j < m+1; j++) {
+	// makes a 2 dimensional array ([n][m]) by mapping to each slot of the initial array (n) another array (m)
+	let lev = Array(n + 1).fill().map(() => Array(m + 1).fill());
+
+	// consider the `i` as the index for the vertial string
+	// considerare le `j` as the index for the horizontal string
+	for (i = 0; i < n + 1; i++) {
+		for (j = 0; j < m + 1; j++) {
 			if (i == 0 || j == 0) {
-				L[i][j] = Math.max(i, j);
+				lev[i][j] = Math.max(i, j); // initial cell for the row || column
 			} else {
-				L[i][j] = Math.min(L[i-1][j] + 1, L[i][j-1] + 1, L[i-1][j-1] + (S.charAt(i-1) != T.charAt(j-1) ? 1 : 0));
+				// sets the distance in a cell according to the minimal edit required to change one string into the other
+				lev[i][j] = Math.min(
+					// which is the min /* between  */ (insertion, deletion, substitution) costs
+					lev[i - 1][j] + 1, lev[i][j - 1] + 1, lev[i - 1][j - 1] + (S.charAt(i - 1) != T.charAt(j - 1) ? 1 : 0)// substitution cost
+				);
 			}
 		}
 	}
-	return L[n][m];
+	return lev[n][m];
 }
 
 /**
